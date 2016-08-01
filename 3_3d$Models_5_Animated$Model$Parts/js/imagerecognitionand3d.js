4fd5b0575b36b6a940ed925720acb71b2a357b49
matt.choi <matt.choi@pacim.local>
2016-08-01 18:23:10 +0800

    data update

--- 3_3d$Models_5_Animated$Model$Parts/js/imagerecognitionand3d.js
+++ 3_3d$Models_5_Animated$Model$Parts/js/imagerecognitionand3d.js
@@ -35,7 +35,7 @@ var World = {
 		*/
 
 
-		this.targetModel = new AR.Model("assets/building_with_word_v3.wt3", {
+		this.targetModel = new AR.Model("assets/skyship.wt3", {
 			onLoaded: this.loadingStep,
 			/*
 				The drawables are made clickable by setting their onClick triggers. Click triggers can be set in the options of the drawable when the drawable is created. Thus, when the 3D model onClick: this.toggleAnimateModel is set in the options it is then passed to the AR.Model constructor. Similar the button's onClick: this.toggleAnimateModel trigger is set in the options passed to the AR.ImageDrawable constructor. toggleAnimateModel() is therefore called when the 3D model or the button is clicked.
@@ -57,7 +57,8 @@ var World = {
 			}
 		});
 
-		this.animationSkyShip = new AR.ModelAnimation(this.targetModel, "Take 001");
+		// this.animationSkyShip = new AR.ModelAnimation(this.targetModel, "Take 001");
+		this.animationSkyShip = new AR.ModelAnimation(this.targetModel, "Animation_00");
 
 		/*
 			As a next step, an appearing animation is created. For more information have a closer look at the function implementation.
