4ce74dd9aa8f1514267dda50b6676727b8bf924a
matt.choi <matt.choi@pacim.local>
2016-09-02 20:00:12 +0800

    model update.

--- 3_3d$Models_5_Animated$Model$Parts/js/imagerecognitionand3d.js
+++ 3_3d$Models_5_Animated$Model$Parts/js/imagerecognitionand3d.js
@@ -10,17 +10,17 @@ var World = {
 	},
 	clearModel: function() {
 		this.tracker.destroy();
-		var i;
-		for (i in this.animations) {
-			this.animations[i].destroy();
-		}
-		var j;
-		for (j in this.targetModels) {
-			this.targetModels[j].destroy();
-		}
-		this.tracker = {};
-		this.targetModels = [];
-		this.animations = [];
+		// var i;
+		// for (i in this.animations) {
+		// 	this.animations[i].destroy();
+		// }
+		// var j;
+		// for (j in this.targetModels) {
+		// 	this.targetModels[j].destroy();
+		// }
+		// this.tracker = {};
+		// this.targetModels = [];
+		// this.animations = [];
 	},
 	loadDayModeAndTracker: function() {
 		this.loadDayModel();
