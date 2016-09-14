9c775b6ad930fe1e978f5ea9bf07ded5e5f38c56
matt.choi <matt.choi@pacim.local>
2016-09-14 17:46:08 +0800

    Sidemen update.

--- 3_3d$Models_5_Animated$Model$Parts/app/wikitudeWorld.js
+++ 3_3d$Models_5_Animated$Model$Parts/app/wikitudeWorld.js
@@ -189,16 +189,6 @@ define(function(require) {
 				animations: this.animations
 			};
 		},
-		loadAnimationWithTarget: function(target) {
-			if (World.loaded && typeof target.model != "undefined") {
-				var i;
-				for (i in target.animationNames) {
-					target.animations.push(new AR.ModelAnimation(target.model, target.animationNames[i]));
-				} catch (err) {
-					console.log(err);
-				}
-			}
-		},
 		loadModel: function(file, _animationNames, isloadAnimations) {
 			var targetModelNight = new AR.Model(file, {
 				onLoaded: this.loadingStep,
@@ -303,6 +293,15 @@ define(function(require) {
 					}
 				}
 			}
+		},
+		loadAnimationWithTarget: function(target) {
+			var i;
+			for (i in target.animationNames) {
+				target.animations.push(new AR.ModelAnimation(target.model, target.animationNames[i]));
+			} catch (err) {
+				console.log(err);
+			}
+
 		},
 		disappear: function() {
 			World.trackableVisible = false;
