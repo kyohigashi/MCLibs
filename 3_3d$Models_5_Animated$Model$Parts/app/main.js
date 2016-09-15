f40ae45ead2e7b6bbdfad65269ed38d299fc639f
matt.choi <matt.choi@pacim.local>
2016-09-15 15:19:32 +0800

    model update.

--- 3_3d$Models_5_Animated$Model$Parts/app/main.js
+++ 3_3d$Models_5_Animated$Model$Parts/app/main.js
@@ -96,7 +96,7 @@ define(function(require) {
                 if (World.modelName != "") {
                     e = "<div" + cssDivLeft + ">Scan Sky100 Tracker Image:</div>" +
                         "<div" + cssDivRight + "><img src='assets/small-icc.jpg'></img><img src='assets/small-icc2.jpg'></img></div>";
-                    $('#selectBuidling').empty().append("<button id='loadBuildingWithText' class='hs-brand-button'>SelectBuilding</button>");
+                    // $('#selectBuidling').empty().append("<button id='loadBuildingWithText' class='hs-brand-button'>SelectBuilding</button>");
                 } else {
                     e = "<div" + cssDivLeft + ">Scan Sky100 Tracker Image:</div>" +
                         "<div" + cssDivRight + "><img src='assets/skyline.jpg'></img></div>";
